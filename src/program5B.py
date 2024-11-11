bfceeaba52d428d859347ebe7c95e3242a2b4b8b
thanizmy <127229049+thanizmy@users.noreply.github.com>
2024-11-11 18:52:51 -0500

    Update program5B.py

--- src/program5B.py
+++ src/program5B.py
@@ -19,8 +19,35 @@ def program5B(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[in
     ############################
     # Add you code here
     ############################
+    #Dp bottom-up approach: Dp[i] table keeps track of the minimum height to arrange the first i paintings. 
+    dp = [float('inf')] * (n+1)  #Intialize dp
+    dp[0] = 0    # Base case: No painting has been arranged.
+    new_display = [-1] * (n+1)   # new_display table to keep track of indices where new display is used 
+    # Starting the arrangement at the first painting to the nth paintign.
+    for i in range(1, n+1):
+        total_width = 0
+        max_height = 0
+        # Iterate to check all possible ways to put the ith paintings with the first j paintings (j <=i). Each possible case recursively calculates the min height for first j-1 paintings. 
+        for j in range(i,0,-1):
+            total_width += widths[j-1]
+            max_height = max(max_height, heights[j-1])
+            # Cannot add ith painting if it exceeds W constraint.
+            if total_width > W:
+                break
+            # If the optimal arrangement up to the j-1 th painting plus the painting from j to i results in smaller heigbt, update dp[i] and store index j-1 as new_display needs.
+            if dp[j-1] + max_height < dp[i]:
+                dp[i] = dp[j-1] + max_height
+                new_display[i] = j-1
+    
+    #Use new_display table to backtrack to find all displays used for optimal arranging all n paintings.
+    displays = []
+    current = n
+    while current > 0:
+        start = new_display[current]
+        displays.append(current - start)
+        current = start
 
-    return 0, 0, [] # replace with your code
+    return len(displays), dp[n], displays[::-1] # revert the right order of backtracking displays used.
 
 
 if __name__ == '__main__':
