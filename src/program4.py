ef4c97397b269b8c09fd97c96ed3b720f103b895
thanizmy <127229049+thanizmy@users.noreply.github.com>
2024-11-11 18:52:15 -0500

    Update program4.py

--- src/program4.py
+++ src/program4.py
@@ -19,8 +19,34 @@ def program4(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int
     ############################
     # Add you code here
     ############################
-
-    return 0, 0, [] # replace with your code
+    # 
+    # O(n^2*W) algorithm: Use dynamic programming approach. 
+    # For each i_th painting, exploring all possible width within constraint W can compute optimal arrangement at each dp[i][w] state.
+    dp = [[(float('inf'), float('inf'), []) for _ in range(W + 1)] for _ in range(n + 1)] # Initialize dp table with size (n+1,W+1)
+    dp[0][0] = (0,0,[])  # Base case: No painting and no width. Each dp element stores its optimal output at that moment.
+    # Iterate throguh all paintings
+    for i in range(1,n+1):
+        #For each painting, iterate through all possible width w up to W.
+        for w in range(W+1):
+            width_sum = 0  # current total width used for that display
+            max_height = 0  # current max height of that dispaly
+            # Iterate through all previously arranged painting to calculate the used width and max height
+            for j in range(i, 0, -1):
+                width_sum += widths[j-1]
+                max_height = max(heights[j-1],max_height)
+                # Stop if total width exceeds width constraint
+                if width_sum > W:
+                    break
+                # Compute height after adding this painting. there are two possible cases:
+                prev_rows, prev_height, prev_num_paintings = dp[j - 1][w]
+                new_height = prev_height + max_height
+                new_paintings_per_row = prev_num_paintings + [i - j + 1]  # New row with painting count
+                # Case 1: The current height is smaller than the optimal height or same height but use fewer rows.Update the new optimal value.
+                if new_height < dp[i][width_sum][1] or (new_height == dp[i][width_sum][1] and prev_rows+1 < dp[i][width_sum][0]):
+                    dp[i][width_sum] = (prev_rows+1, new_height, new_paintings_per_row)
+                # Case 2: Otherwise, we will not consider that painting as it is not optimal choice there. Continue considering other width choices. 
+    min_rows, min_height, num_paintings = min((dp[n][w] for w in range(W+1)), key = lambda x: (x[0], x[1]))  # Find the smallest height and fewest rows in all possible arrangements in the dp table. It is the optimal value.
+    return min_rows, min_height, num_paintings # replace with your code
 
 
 if __name__ == '__main__':
